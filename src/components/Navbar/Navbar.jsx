2ba94c46aa29cf7aa69fe887e0c077a5d0f6faef
Aniket <aniketdhokane2071@gmail.com>
2022-01-24 16:20:54 +0530

    Added Link react-router-dom

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css'
+// import { Link } from 'react-router-dom';
 
 import {
     BrowserRouter as Router,
@@ -14,11 +15,11 @@ const Navbar = () => {
     <div className="navbar1">
     <nav className="navbar navbar-expand-lg navbar-dark" id="navbar" style={{"background-color": "#FE2C54"}}> 
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+            <Link to="/" className="navbar-brand" >
                 <div className="logo-container">
                     Covid Relief
                 </div>
-            </a>
+            </Link>
 
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -27,41 +28,41 @@ const Navbar = () => {
             <div className="collapse navbar-collapse m-2" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li className="nav-item m-1 pb-1 ">
-                    <a className="nav-link active" aria-current="page" href="#">
+                    <Link to="/plasma" className="nav-link active" aria-current="page" >
                             <span className="material-icons">bloodtype</span>
                             Plasma Donors
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item m-1 pb-1 ">
-                        <a className="nav-link active" aria-current="page" href="./hospital-beds-list-page.html">
+                        <Link to="/hospitalbeds" className="nav-link active" aria-current="page" >
                             <span className="material-icons">local_hospital</span>
                             Hospital Beds
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item m-1 pb-1 ">
-                        <a className="nav-link active" aria-current="page" href="./oxygen-suppliers-list-page.html">
+                        <Link to="/oxygensupply" className="nav-link active" aria-current="page" >
                             <span className="material-icons">air</span>
                             Oxygen Suppliers
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item m-1 pb-1 ">
-                        <a className="nav-link active" aria-current="page" href="./meds-injection-suppliers-list-page.html">
+                        <Link to="/medssupply" className="nav-link active" aria-current="page" >
                             <span className="material-icons">medication</span>
                             Meds Suppliers
-                        </a>
+                        </Link>
                     </li>
                     <li className="nav-item m-1 pb-1 ">
-                        <a className="nav-link active" aria-current="page" href="./ambulance-providers-list-page.html">
+                        <Link to="/ambulanceprovider" className="nav-link active" aria-current="page" >
                             <span className="material-icons">airport_shuttle</span>
                             Ambulance Providers
-                        </a>
+                        </Link>
                     </li>
-                    <li className="nav-item m-1 pb-1 ">
-                        <a className="nav-link active" aria-current="page" href="./tiffin-service-list-page.html">
+                    {/* <li className="nav-item m-1 pb-1 ">
+                        <a className="nav-link active" aria-current="page" >
                             <span className="material-icons">takeout_dining</span>
                             Tiffin Service
                         </a>
-                    </li>
+                    </li> */}
 
                     <li className="nav-item dropdown m-1 pb-1">
                         <a className="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
