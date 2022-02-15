8b81aed56b368cde71a6acd0a8cab719f32c7872
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-02-15 22:13:45 +0530

    added schema for plasma

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,37 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
+const morgan  = require("morgan")
+const PlasmaRoutes = require("./routes/Plasma")
 
 const PORT = 5000;
 
+const dotenv = require("dotenv")
+dotenv.config();
+
 app.get("/" , (req,res) => {
-    res.send("backend is connected")
+    res.send("backend is connected",{useNewUrlParser: true , useUnifiedTopology: true})
 })
 
-/* mongoose.connect("") */
+mongoose.connect(process.env.MONGO_URL_CONNECTION).then(()=>{
+    console.log("mongodb is connected")
+}).catch((err) => {
+    console.log(err)
+})
+
+
+/* middleware */
+app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
+
+/* routes */
+app.use("/api" , PlasmaRoutes)
+
+
 
 
 app.listen(PORT , () => {
